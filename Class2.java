1162f8f80e560ee48b017e44d4c7a4a8f8622301
SaiJaswanth91 <gundrajusaijaswanth@gmail.com>
2025-07-07 19:59:57 +0530

    wipro_talentnext

---
+++ Class2.java
@@ -0,0 +1,12 @@
+package project2;
+import java.util.Scanner;
+public class Class2 {
+
+	public static void main(String[] args) 
+	{
+	Scanner as=new Scanner(System.in);
+	String a =as.nextLine();
+	System.out.println("Welcome "+a);
+	}
+
+}
--- ClassA.java
+++ ClassA.java
@@ -1,14 +1,17 @@
-package com.Package;
+package project6;
 import java.util.*;
-
 public class ClassA
 {
-    public static void main(String[] args) 
-    {
-      Scanner d=new Scanner(System.in);
-      String a = d.nextLine();
-      
-      String b = d.nextLine();
-       System.out.println(a + " Technologies " + b);
-    }
+
+	public static void main(String[] args)
+	{
+		Scanner x=new Scanner(System.in);
+		int g =x.nextInt();
+		if(g%2==0)
+			System.out.println("Its even number");
+		else 
+			System.out.println("Its odd number");
+
+	}
+
 }
